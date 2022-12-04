073df56b46d991c8dfba89ad36591cdf5b1e13c4
Ben Bld <bld.prom@gmail.com>
2022-12-04 13:38:04 +0100

    chore : created seperate functions

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,19 +10,20 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelectorAll(".close");
 
-
-const form = document.querySelector("#reserve");
+const form = document.querySelector("form");
+const formData = document.querySelectorAll(".formData");
 const firstName = document.querySelector("#first");
 const lastName = document.querySelector("#last");
 const email = document.querySelector("#email");
 const birthDay = document.querySelector("#birthdate");
-
 const pastTournament = document.querySelector("#quantity");
-const placeTournament = document.querySelectorAll(".location");
+const tournamentPlace = document.querySelectorAll('input[name="location"]');
 const consent = document.querySelector("#checkbox1");
+
+const submitForm = document.querySelector('.btn-submit');
+
 
 
 
@@ -49,88 +50,33 @@ form.addEventListener("submit", e => {
 })
 
 function formSubmit() {
-  // obtenir toutes les valeurs des inputs
-  const firstNameValue = firstName.value.trim();
-  const lastNameValue = lastName.value.trim();
-  const emailValue = email.value.trim();
-  const birthDayValue = birthDay.valueAsDate;
-  const pastTournamentValue = pastTournament.valueAsNumber;
-  const placeTournamentValue = placeTournament.checked; 
-  const consentValue = consent.checked;
-
-  // first name verify
-    if (firstNameValue === "" ) {
-      let message = "Veuillez remplir le champ du prénom.";
-      setError(firstName, message);
-    } else if (!firstNameValue.match(/^[a-zA-Z-éèà\s]+$/)) {
-      let message = "Le champ du prénom doit contenir uniquement des lettres.";
-      setError(firstName, message);
-    } else if (firstNameValue.length < 2) {
-        let message = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-        setError(firstName, message);
-    } else {
-      setSuccess(firstName);
-    }
+  let isValid = true ;
   
-  //last name verify
-    if (lastNameValue === "" ) {
-      let message = "Veuillez remplir le champ du nom.";
-      setError(lastName, message);
-    } else if (!lastNameValue.match(/^[a-zA-Z-éèà\s]+$/)) {
-      let message = "Le champ du nom doit contenir uniquement des lettres.";
-      setError(lastName, message);
-    } else if (lastNameValue.length < 2) {
-        let message = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-        setError(lastName, message);
-    } else {
-      setSuccess(lastName);
-    }
-
-//email verify
-    if (emailValue === "" ) {
-      let message = "Veuillez remplir le champ de l'adresse mail.";
-      setError(email, message);
-    } else if(!emailVerify(emailValue)) {
-      let message = "Veuillez saisir une adresse mail valide.";
-      setError(email, message);
-    } else {
-      setSuccess(email);
-    }
+  if (!firstNameVerify()) {
+    isValid = false;
+  } 
+  if (!lastNameVerify()) {
+    isValid = false;
+  }
+  if (!emailVerify()) {
+    isValid = false;
+  }
+  if (!birthDayVerify()) {
+    isValid = false;
+  }
+  if (!pastTournamentVerify()) {
+    isValid = false;
+  }
 
 
-//birthday verify
-    if (birthDayValue === null ) {
-      let message = "Veuillez indiquer votre date de naissance.";
-      setError(birthDay, message); 
-    } else if(birthDayValue > Date.now()) {
-      let message = "Veuillez saisir une date de naissance valide.";
-      setError(birthDay, message);
-    } else if(birthDayValue < Date.parse(birthDay.min)) {
-      let message = "La date saisie est trop ancienne.";
-      setError(birthDay, message);
-    } 
-    
-    else {
-      setSuccess(birthDay);
-    }
-
-
-//past tournaments participation verify
-    if (!pastTournamentVerify(pastTournamentValue)) {
-      let message = "Veuillez saisir une valeur numérique.";
-      setError(pastTournament, message);
-    } else if (pastTournamentValue > 99) {
-      let message = "Veuillez saisir une valeur inférieure ou égale à 99.";
-      setError(pastTournament, message);
-    } else {
-      setSuccess(pastTournament);
-    }
-
-
+//   const tournamentPlaceValue = tournamentPlace.checked;
+//   const consentValue = consent.checked;
 
 
 
 }
+
+
 
 
 
@@ -153,13 +99,67 @@ function setError(elem,message) {
   formData.className = "formData error";
 }
 
-//fonction succès
+//fonction message succès
 function setSuccess(elem) {
   const formData = elem.parentElement;
   formData.className = "formData success";
 }
 
-function emailVerify(email) {
+// first name verify
+function firstNameVerify() {
+  const firstNameValue = firstName.value.trim();
+  
+  if (firstNameValue === "" ) {
+    let message = "Veuillez remplir le champ du prénom.";
+    setError(firstName, message);
+  } else if (!firstNameValue.match(/^[a-zA-Z-éèà\s]+$/)) {
+      let message = "Le champ du prénom doit contenir uniquement des lettres.";
+      setError(firstName, message);
+  } else if (firstNameValue.length < 2) {
+      let message = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+      setError(firstName, message);
+  } else {
+      setSuccess(firstName);
+  }
+}
+
+
+  //last name verify
+function lastNameVerify() {
+  const lastNameValue = lastName.value.trim();
+
+  if (lastNameValue === "" ) {
+    let message = "Veuillez remplir le champ du nom.";
+    setError(lastName, message);
+  } else if (!lastNameValue.match(/^[a-zA-Z-éèà\s]+$/)) {
+      let message = "Le champ du nom doit contenir uniquement des lettres.";
+      setError(lastName, message);
+  } else if (lastNameValue.length < 2) {
+      let message = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+      setError(lastName, message);
+  } else {
+      setSuccess(lastName);
+  }
+}
+
+
+
+// email verify
+function emailVerify() {
+  const emailValue = email.value.trim();
+
+  if (emailValue === "" ) {
+    let message = "Veuillez remplir le champ de l'adresse mail.";
+    setError(email, message);
+  } else if(!emailRegex(emailValue)) {
+      let message = "Veuillez saisir une adresse mail valide.";
+      setError(email, message);
+  } else {
+      setSuccess(email);
+  }
+}
+
+function emailRegex(email) {
   /*  Adresse mail valide : xyz.abc@example.com
       Format : "lettres/chiffres._-"  + "@" + "lettres/chiffres" + "." + "lettres"   */
   return (
@@ -167,28 +167,64 @@ function emailVerify(email) {
   );
 }
 
-function pastTournamentVerify(pastTournament) {
+
+//birthday verify
+function birthDayVerify() {
+  const birthDayValue = birthDay.valueAsDate;
+
+  if (birthDayValue === null ) {
+    let message = "Veuillez indiquer votre date de naissance.";
+    setError(birthDay, message); 
+  } else if(birthDayValue > Date.now()) {
+      let message = "Veuillez saisir une date de naissance valide.";
+      setError(birthDay, message);
+  } else if(birthDayValue < Date.parse(birthDay.min)) {
+      let message = "La date saisie est trop ancienne.";
+      setError(birthDay, message);
+  } else {
+      setSuccess(birthDay);
+  }
+}
+
+/* set max birthdate input to today
+
+  let today = Date.now();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //January is 0!
+  let yyyy = today.getFullYear();
+  if (dd < 10) {
+     dd = '0' + dd;
+   }
+
+   if (mm < 10) {
+     mm = '0' + mm;
+   } 
+
+   today = yyyy + '-' + mm + '-' + dd;
+  document.getElementById("birthdate").setAttribute("max", today);
+*/
+
+
+//past tournament participation verify
+function pastTournamentVerify() {
+  const pastTournamentValue = pastTournament.valueAsNumber;
+  
+  if (!pastTournamentRegex(pastTournamentValue)) {
+    let message = "Veuillez saisir une valeur numérique.";
+    setError(pastTournament, message);
+  } else if (pastTournamentValue > 99) {
+      let message = "Veuillez saisir une valeur inférieure ou égale à 99.";
+      setError(pastTournament, message);
+  } else {
+      setSuccess(pastTournament);
+  }
+}
+
+function pastTournamentRegex(pastTournament) {
   return (
     /^[0-9]+$/.test(pastTournament)
   );
 }
 
 
-
-// set max birthdate input to today
-
-//  let today = Date.now();
-//  let dd = today.getDate();
-//  let mm = today.getMonth() + 1; //January is 0!
-//  let yyyy = today.getFullYear();
-
-//   if (dd < 10) {
-//     dd = '0' + dd;
-//   }
-
-//   if (mm < 10) {
-//     mm = '0' + mm;
-//   } 
-    
-//   today = yyyy + '-' + mm + '-' + dd;
-//  document.getElementById("birthdate").setAttribute("max", today);
+// tournament location verify
